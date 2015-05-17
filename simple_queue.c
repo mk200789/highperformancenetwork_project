6cae23b754b17b6bd73c2cd12fc7cab60db87cde
Wan Kim Mok <cafe.mui@gmail.com>
2015-05-17 02:02:39 -0400

    include display_queue()

--- simple_queue.c
+++ simple_queue.c
@@ -22,15 +22,23 @@ int x;
 	}
 }
 
-void remove_queue(x)
-int x;
+void remove_queue()
 {
 	if (front == -1 || front > rear){
 		printf("underfow\n");
 	}
 	else{
+		queue_array[front] = 0;
 		front = front + 1;
 	}
+}
+
+void display_queue(){
+	printf("|");
+	for (int i=0; i<=rear; i++){
+		printf("%d| ", queue_array[i]);
+	}
+	printf("\n");
 }
 
 
@@ -38,9 +46,12 @@ int main(){
 	printf("Size of the array is: %lu\n", sizeof(queue_array));
 	insert_queue(10);
 	insert_queue(40);
-	printf("|");
-	for (int i=0; i<=rear; i++){
-		printf("%d| ", queue_array[i]);
-	}
-	printf("\n");
+	display_queue();
+	remove_queue();
+	display_queue();
+	insert_queue(15);
+	insert_queue(45);
+	display_queue();
+	remove_queue();
+	display_queue();
 }
