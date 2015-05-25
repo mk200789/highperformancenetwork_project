6ccd9e17a292a64bc94012653c0fa699bc23ed8f
Wan Kim Mok <cafe.mui@gmail.com>
2015-05-25 02:53:58 -0400

    modified leaky bucket

--- leaky_bucket.c
+++ leaky_bucket.c
@@ -2,64 +2,107 @@
 Implementing a basic leaky bucket algorithm.
 */
 
+/*
+INCLUDES
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
+/*
+DEFINES
+*/
+#define INPUT_FILE "jurrassic_1.txt"
+#define OUTPUT_FILE "shape.txt"
+#define BUCKET_SIZE 5000
+#define TRUE 1
+#define FALSE 0
 
-//int time = 0;
-int current_amt = 0;
+/*
+PROTOTYPES
+*/
+int bucketInput(int, int);
 
-void bucketInput(int a, int b, int size){
-	current_amt += a;
-	if (current_amt > size){
+/* 
+GLOBAL
+*/
+int counter = 0;
+
+int bucketInput(int a, int b){
+	if (a > BUCKET_SIZE){
 		printf("\tBucket overflow.\n");
-		current_amt -= a;
+		return TRUE;
 	}
 	else{
-		//time++;
-		current_amt -= a;
 		while(a>b){
-			//printf("Time: %d seconds\n", time);
 			printf("\t%d bytes outputted\n", b);
 			a -=b;
-			//time++;
+			counter++;
+			sleep(1);
 		}
-		//printf("Time: %dseconds\n", time);
+
 		if (a>0)
 			printf("\tLast %d bytes sent", a);
 		printf("\tBucket output successful.\n");
 		
-
+	return FALSE;
 	}
 
 }
 
+
 int main(){
-	FILE *fp;
-	int time_ms, length;
-	int leakRate, packetSize, bucketSize;
+	time_t start_time;
+	FILE *fp, *sp;
+	int time_ms, length, i, old_counter, current_time, overflow;
+	int leakRate, packetSize, delay;
 
-	fp = fopen("jurrassic_1.txt", "r");
+	fp = fopen(INPUT_FILE, "r");
+	sp = fopen(OUTPUT_FILE, "w");
+	//fprintf(sp, "Packet No. Delay Packet Size\n");
 
-	printf("Bucket Size (in byte, i.e 8000): ");
-	scanf("%d", &bucketSize);
 	printf("Leak Rate: ");
 	scanf("%d", &leakRate);
 
+	
 	if (fp == NULL){
 		fprintf(stderr, "Can't open input file jurassic.txt.\n");
 		exit(1);
 	}
 	else{
-		int i = 0;
+		i = 1;
+		old_counter = 0;
+		start_time = time(NULL);
 		while(fscanf(fp, "%d %d", &time_ms, &length) != EOF){
+			current_time = (time(NULL)-start_time);
+			counter = 0;
+			while (current_time != time_ms/40 && time_ms <=current_time){
+				printf("current_time %ld\n", (time(NULL)-start_time));
+				printf("time_ms %d\n", time_ms/40);
+				printf("hello\n");
+				counter++;
+				sleep(1);
+				current_time = (time(NULL)-start_time);
+				//printf("looping %d %d\n", current_time, time_ms/40);
+			}
+
+			//printf("bingo %d %d\n", current_time, time_ms/40);
+			//old_counter = counter;
 			packetSize = length;
 			printf("Packet no %d\t Packet size = %d\n", i, packetSize);
+			overflow = bucketInput(packetSize, leakRate);
+			if (overflow == 0){
+				fprintf(sp, "%d\t\t\t%d\t\t\t%d\n", i, (int)(counter-old_counter), packetSize);
+			}
+			else{
+				fprintf(sp, "%d\t\t\t%s\t\t\t%d\n", i, "-", packetSize);
+			}
 			i++;
-			bucketInput(packetSize, leakRate, bucketSize);
 		}
+
 	}
 	fclose(fp);
+	fclose(sp);
 	return 0;
 }
